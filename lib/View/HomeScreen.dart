b3346c243ffd7ba67c259ebd549a5ab98dbca160
MAR miju <mar.miju.dev@gmail.com>
2024-12-30 00:10:44 +0600

    Updated

--- lib/View/HomeScreen.dart
+++ lib/View/HomeScreen.dart
@@ -3,24 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Homescreen extends ConsumerWidget {
-   
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userData = ref.watch(UserProvider);
     return Scaffold(
+      appBar: AppBar(
+        title: Text('JsonPlaceHolder/User'),
+        
+      ),
        body:userData.when(
         data: (data)=>ListView.builder(
           itemCount: data.length,
           itemBuilder: (_,index){
             //sapareate user One by One
             final user = data[index];
-           return Card(
-             child: ListTile(
-              title: Text('${user.company.name} \n ${user.company.bs}'),
-              leading: Text('${user.name} \n ${user.email}'),
-             
-             
+           return Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 16),
+             child: Card(
+               child: ListTile(
+                leading: Text('Id:${user.id}'),
+                title: Text('Name: ${user.name}',style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),),
+                subtitle: Text('Company Name:${user.company.name} \nEmail: ${user.email}'),
+                
+               ),
              ),
            );
 
